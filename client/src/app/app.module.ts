72f79438e6a1239c5d2913223517675f6c45faeb
danielcollette <daniel.collette@pb.com>
2018-09-17 08:59:26 -0400

    update libs, add loading bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+
 import { AppRoutingModule } from '@app/app-routing.module';
 
 import { AppComponent } from '@app/app.component';
@@ -9,8 +12,8 @@ import { SharedModule } from '@app/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, LoadingBarHttpClientModule, LoadingBarRouterModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
