23fbc987c2187ec7c5f16eb8765ffd6332bd6df3
danielcollette <daniel.collette@pb.com>
2018-11-01 14:59:41 -0400

    updated to accessible header

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  @HostBinding('class.site-header') true;
+  @HostBinding('class.site-header')
+  true;
+
+  @ViewChild('searchLink')
+  searchLink: ElementRef;
+
+  @ViewChild('searchInput')
+  searchInput: ElementRef;
+
   public searchActive: boolean;
 
   constructor() {
     this.searchActive = false;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
-  toggleSearch = function() {
+  toggleSearch = function ($event: Event) {
+    $event.preventDefault();
+
     if (this.searchActive) {
       this.searchActive = false;
+
+      setTimeout(() => {
+        this.searchLink.nativeElement.focus();
+      }, 0);
     } else {
       this.searchActive = true;
+
+      setTimeout(() => {
+        this.searchInput.nativeElement.focus();
+      }, 0);
     }
   };
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent, FooterComponent, HeaderShadowDirective } from '@app/shared';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, RouterModule],
   declarations: [HeaderComponent, FooterComponent, HeaderShadowDirective],
   exports: [HeaderComponent, FooterComponent, HeaderShadowDirective]
 })
-export class SharedModule { }
+export class SharedModule {}
