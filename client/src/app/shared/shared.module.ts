6455c5353c78a18cf0261d8d5656ec1411089143
Steve Lombardi <steve.lombardi@pb.com>
2019-06-27 11:29:40 -0400

    remove old directive from header, add new one

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
-import { HeaderComponent, FooterComponent, HeaderShadowDirective } from '@app/shared';
+import { FooterComponent } from '@app/shared/footer/footer.component';
+import { HeaderComponent } from '@app/shared/header/header.component';
+import { PbdsHeaderShadowModule } from 'pb-design-system';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, RouterModule],
-  declarations: [HeaderComponent, FooterComponent, HeaderShadowDirective],
-  exports: [HeaderComponent, FooterComponent, HeaderShadowDirective, FormsModule, CommonModule]
+  imports: [CommonModule, FormsModule, RouterModule, PbdsHeaderShadowModule],
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, PbdsHeaderShadowModule]
 })
 export class SharedModule {}
