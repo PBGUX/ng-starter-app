f18d3c40e22476625843ace6ddbb0b0082b7319e
Steve Lombardi <steve.lombardi@pb.com>
2020-09-10 13:55:26 -0400

    remove localize
    Signed-off-by: Steve Lombardi <steve.lombardi@pb.com>

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -1,8 +1,7 @@
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
-import '@angular/localize/init';
-
+// import '@angular/localize/init';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
@@ -21,9 +20,9 @@ import '@angular/localize/init';
 // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
 
 /*
-* in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
-* with the following flag, it will bypass `zone.js` patch for IE/Edge
-*/
+ * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
+ * with the following flag, it will bypass `zone.js` patch for IE/Edge
+ */
 // (window as any).__Zone_enable_cross_context_check = true;
 
 /***************************************************************************************************
