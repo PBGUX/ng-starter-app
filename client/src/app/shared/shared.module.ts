b1aa0987f67f7c4b3c36530571c731c1e44a5ae0
Steve Lombardi <steve.lombardi@pb.com>
2020-09-10 13:22:38 -0400

    page title
    Signed-off-by: Steve Lombardi <steve.lombardi@pb.com>

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 import { FooterComponent } from '@app/shared/footer/footer.component';
 import { HeaderComponent } from '@app/shared/header/header.component';
 import { PbdsHeaderShadowModule } from 'pb-design-system';
+import { PageTitleComponent } from './page-title.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, RouterModule, PbdsHeaderShadowModule],
-  declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, PbdsHeaderShadowModule]
+  declarations: [HeaderComponent, FooterComponent, PageTitleComponent],
+  exports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, PbdsHeaderShadowModule, PageTitleComponent]
 })
 export class SharedModule {}
