6d387032e638465c940af61d30f8f05287f386f4
Steve Lombardi <steve.lombardi@pb.com>
2021-04-29 12:15:37 -0400

    updates
    Signed-off-by: Steve Lombardi <steve.lombardi@pb.com>

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  @HostBinding('attr.role') attrRole = 'main';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
