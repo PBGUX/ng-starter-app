a6bf132a62a7f7e430b793db154ca6a3299d2777
Steve Lombardi <steve.lombardi@pb.com>
2022-01-04 09:54:34 -0500

    code cleanup

--- client/src/app/shared/footer/footer.component.ts
+++ client/src/app/shared/footer/footer.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
 })
 export class FooterComponent implements OnInit {
-  @HostBinding('class.site-footer')
-  @HostBinding('class.fixed-bottom')
-  true;
+  @HostBinding('class.site-footer') true;
   private startYear = 2018;
   public date: string;
 
-  constructor() {}
-
-  ngOnInit() {
+  ngOnInit(): void {
     const currentYear: number = new Date().getFullYear();
     this.date = this.startYear === currentYear ? `${currentYear}` : `${this.startYear}-${currentYear}`;
   }
